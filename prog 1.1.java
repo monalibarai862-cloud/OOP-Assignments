e4516773e19cfa8281fe91b707428006761cb585
monalibarai862 <monalibarai862@gmail.com>
2026-03-17 20:29:52 +0530

    Added OOP assignment

---
+++ prog 1.1.java
@@ -0,0 +1,27 @@
+class Person {
+    String name;
+    int age;
+
+    // Constructor
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to display details
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        // Creating two Person objects
+        Person p1 = new Person("Anushka", 21);
+        Person p2 = new Person("Kashish", 23);
+
+        // Printing their details
+        p1.display();
+        p2.display();
+    }
+}
