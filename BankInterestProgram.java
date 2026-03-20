3f6cdb3a5aba7607038fbb05939e80318350637b
monalibarai862 <monalibarai862@gmail.com>
2026-03-20 20:45:08 +0530

    interest calculate

---
+++ BankInterestProgram.java
@@ -0,0 +1,24 @@
+public class BankInterestProgram {
+
+    // Bank class
+    static class Bank {
+        final double interestRate = 5.0; // fixed interest rate
+
+        double calculateInterest(double amount) {
+            return (amount * interestRate) / 100;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Bank bank = new Bank();
+
+        double customer1 = 10000;
+        double customer2 = 20000;
+        double customer3 = 5000;
+
+        System.out.println("Interest for Customer 1: ₹" + bank.calculateInterest(customer1));
+        System.out.println("Interest for Customer 2: ₹" + bank.calculateInterest(customer2));
+        System.out.println("Interest for Customer 3: ₹" + bank.calculateInterest(customer3));
+    }
+}
--- EmployeeCountDemo.java
+++ EmployeeCountDemo.java
@@ -1,30 +1,22 @@
 public class EmployeeCountDemo {
 
-    static int employeeCount = 0; // static variable
+    static int employeeCount = 0; 
 
     String name;
     int id;
-
-    // Constructor
     EmployeeCountDemo(String name, int id) {
         this.name = name;
         this.id = id;
-        employeeCount++; // increment count
+        employeeCount++;
     }
-
-    // Static method to display count
     static void showCount() {
         System.out.println("Total Employees: " + employeeCount);
     }
 
     public static void main(String[] args) {
-
-        // Creating objects
         EmployeeCountDemo e1 = new EmployeeCountDemo("Anushka", 101);
         EmployeeCountDemo e2 = new EmployeeCountDemo("Rahul", 102);
         EmployeeCountDemo e3 = new EmployeeCountDemo("Priya", 103);
-
-        // Display total employees
         EmployeeCountDemo.showCount();
     }
 }
