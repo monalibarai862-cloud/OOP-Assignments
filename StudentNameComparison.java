b2d2d15faa56a89a9bf07b638f00fb5dcd1f17b5
monalibarai862 <monalibarai862@gmail.com>
2026-03-22 16:42:32 +0530

    case sensitive-insensitive comparison

---
+++ StudentNameComparison.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class StudentNameComparison {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first student name: ");
+        String name1 = sc.nextLine();
+
+        System.out.print("Enter second student name: ");
+        String name2 = sc.nextLine();
+
+        if (name1.equals(name2)) {
+            System.out.println("Case-Sensitive: Names are SAME");
+        } else {
+            System.out.println("Case-Sensitive: Names are DIFFERENT");
+        }
+        if (name1.equalsIgnoreCase(name2)) {
+            System.out.println("Case-Insensitive: Names are SAME");
+        } else {
+            System.out.println("Case-Insensitive: Names are DIFFERENT");
+        }
+
+        sc.close();
+    }
+}
