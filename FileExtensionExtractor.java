a3b6ea6ef51ed5c61a09a11b853d440a656187c2
monalibarai862 <monalibarai862@gmail.com>
2026-03-22 16:49:09 +0530

    Substring

---
+++ FileExtensionExtractor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class FileExtensionExtractor {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter file name: ");
+        String fileName = sc.nextLine();
+        int index = fileName.lastIndexOf('.');
+
+        if (index != -1) {
+            String extension = fileName.substring(index + 1);
+            System.out.println("File Extension: " + extension);
+        } else {
+            System.out.println("No extension found");
+        }
+
+        sc.close();
+    }
+}
