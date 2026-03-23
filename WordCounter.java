c09b6e7ca3adb4445ea513322236a6d328966a9c
monalibarai862 <monalibarai862@gmail.com>
2026-03-23 22:56:50 +0530

    Buffer

--- WordCounter.java
+++ WordCounter.java
@@ -5,16 +5,13 @@ public class WordCounter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Accept sentence from user
         System.out.print("Enter a sentence: ");
         String sentence = sc.nextLine();
-
-        // Use StringTokenizer to count words
+        
         StringTokenizer st = new StringTokenizer(sentence);
 
         int count = st.countTokens();
 
-        // Display result
         System.out.println("Total number of words: " + count);
 
         sc.close();
