d99388a1dab2d5cf27ae2f6c74cb5254b29dfa4c
monalibarai862 <monalibarai862@gmail.com>
2026-03-23 23:03:10 +0530

    toString Override

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Student ID: " + id + ", Name: " + name;
+    }
+    public static void main(String[] args) {
+
+        Student s1 = new Student(101, "Ram");
+
+        System.out.println(s1);  
+    }
+}
