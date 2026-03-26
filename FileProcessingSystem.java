503a20f877a798563268f36c6a7344c438100b7a
monalibarai862 <monalibarai862@gmail.com>
2026-03-26 17:00:32 +0530

    File processing

---
+++ FileProcessingSystem.java
@@ -0,0 +1,15 @@
+class FileProcessingSystem {
+    public static void main(String[] args) {
+
+        try {
+            
+            System.out.println("Reading file...");
+
+        } catch (Exception e) {
+            System.out.println("Error occurred while reading file");
+        } finally {
+            // Cleanup code
+            System.out.println("Closing file resources");
+        }
+    }
+}
