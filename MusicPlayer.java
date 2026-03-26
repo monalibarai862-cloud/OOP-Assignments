7fb4f20f14ca5d8f80d82bee8c28172f5d96a294
monalibarai862 <monalibarai862@gmail.com>
2026-03-26 17:07:24 +0530

    Thread class

---
+++ MusicPlayer.java
@@ -0,0 +1,23 @@
+class MusicPlayer extends Thread {
+
+    public void run() {
+        try {
+            System.out.println("Playing song...");
+
+            for (int i = 1; i <= 3; i++) {
+                Thread.sleep(1000); 
+                System.out.println("Progress " + i);
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MusicPlayer t = new MusicPlayer();
+
+        t.start();
+    }
+}
